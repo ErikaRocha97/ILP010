02479152330e19187cd71562acf587f8e1a3461c
Erika Rocha <erikaarosio@hotmail.com>
2025-04-08 10:15:16 -0300

    Update atv-06-01.c

--- Aula-06/atv-06-01.c
+++ Aula-06/atv-06-01.c
@@ -1,23 +1,13 @@
-// Codifique um programa de teste para a rotina max( ).
-
-// double max (double a, double b) {
-//   if ( a>b )
-//      return a;/* interrompe a função*/
-//   return b;
-// }
-
 #include <stdio.h>
 
 double max (double a, double b) {
     if (a > b)
         return a;
-    return b;
-    
+    return b;  
 }
 
 int main()
 {
     printf("%.1lf", max(5,10));
-
     return 0;
 }
